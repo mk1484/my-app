71b787b1d7ccc172c3fbbb1b0f5948d5612d61d9
mk1484 <flat.yz14@gmail.comm>
2021-02-03 03:28:58 +0900

    ボタンの作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Btn from './Btn';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Btn />
     </div>
   );
 }
