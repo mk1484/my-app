aaefd138c4616ca9893374693a19741fd9b32cb7
mk1484 <flat.yz14@gmail.com>
2021-03-07 23:55:42 +0900

    ボタンクリックでテキストの表示 (#4)
    Co-authored-by: mk1484 <flat.yz14@gmail.comm>

--- src/Btn.js
+++ src/Btn.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react"
 
 const Btn = () => {
-    const [toggle, setToggle] = useState(false)
+    const [toggle, setToggle] = useState(false);
+
+
 
     return (
         <div>
             <button onClick={() => setToggle(!toggle)}>ボタン</button>
             <div>{console.log(toggle)}</div>
+            {toggle && <p>クリックしました</p>}
         </div>
     )
 }
