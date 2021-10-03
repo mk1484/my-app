24448c87f08db67e186cbb83768262de53c9f23f
mk1484 <flat.yz14@gmail.com>
2021-10-04 01:27:00 +0900

    テキストコンポーネントのpropsを分割代入

--- src/Text.jsx
+++ src/Text.jsx
@@ -3,22 +3,24 @@ import React from "react"
 const Text = (props) => {
   // console.log(props);
 
+  const { message, color, children } = props;
+
   const contentPinkStyle = {
     color: "pink",
     fontSize: "20px"
   };
 
   const contentStyle = {
-    color: props.color,
+    color: color,
     fontSize: "20px"
   };
 
     return (
       <div>
         <h1>こんにちは！</h1>
-        <p style={contentStyle}>{props.message}</p>
+        <p style={contentStyle}>{message}</p>
         <p style={contentPinkStyle}>元気です！</p>
-        <p style={contentStyle}>{props.children}</p>        
+        <p style={contentStyle}>{children}</p>        
       </div>
     );
 };
