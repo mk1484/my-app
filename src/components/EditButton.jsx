0db446abb44f8e12fd217df55f4d90c314f1c982
mk1484 <flat.yz14@gmail.com>
2021-10-23 03:18:15 +0900

    ContextでState管理をする (#29)

---
+++ src/components/EditButton.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AdminFlagContext } from "./providers/AdminFlagProvider";
+
+const style = {
+  width: "100px",
+  padding: "6px",
+  borderRadius: "8px",
+};
+
+export const EditButton = () => {
+    const { isAdmin } = useContext(AdminFlagContext);
+    const contextValue = useContext(AdminFlagContext);
+    console.log(contextValue);
+
+    return (
+        <button style={style} disabled={!isAdmin}>
+          編集
+        </button>
+    )
+};
+
+export default EditButton;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AdminFlagProvider } from "./components/providers/AdminFlagProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AdminFlagProvider>
+      <App />
+    </AdminFlagProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
